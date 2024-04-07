78fc458522cecdfd9f4a8b2edb3d34380198e975
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-04-07 20:03:24 +1200

    `Enumerator` is currently blocking - fix merged for Ruby 3.4+.

--- app/controllers/streaming_controller.rb
+++ app/controllers/streaming_controller.rb
@@ -1,15 +1,20 @@
 class StreamingController < ApplicationController
+  PREFIX = (("." * 1024) + "\n").freeze
+  
   def index
-    body = Enumerator.new do |enumerator|
-      100.downto(1) do |i|
-        enumerator << "#{i} bottles of beer on the wall\n"
-        sleep 0.1
-        enumerator << "#{i} bottles of beer\n"
-        sleep 0.1
-        enumerator << "Take one down, pass it around\n"
-        sleep 0.1
-        enumerator << "#{i - 1} bottles of beer on the wall\n"
-        sleep 0.1
+    body = proc do |stream|
+      # This gets the browser to start rendering interactively:
+      stream.write(PREFIX)
+      
+      10.downto(1) do |i|
+        stream.write "#{i} bottles of beer on the wall\n"
+        sleep 1
+        stream.write "#{i} bottles of beer\n"
+        sleep 1
+        stream.write "Take one down, pass it around\n"
+        sleep 1
+        stream.write "#{i - 1} bottles of beer on the wall\n"
+        sleep 1
       end
     end
 
