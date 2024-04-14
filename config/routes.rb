90bb84230f010ceeeac0633c8e5fcf359d4ca5d1
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-04-14 13:51:12 +1200

    Fix ollama routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'ollama/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "welcome#index"
-
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
